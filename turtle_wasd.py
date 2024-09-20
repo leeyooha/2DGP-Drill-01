5115efeab91352af22c4593a1e845a518b6a572b
yoohalee@TIP209 <yooha0330@naver.com>
2024-09-20 11:50:34 +0900

    Drill 1 제출 (2021180047 이유하)

---
+++ turtle_wasd.py
@@ -0,0 +1,27 @@
+import turtle
+
+
+def move_up():
+    turtle.setheading(90)
+    turtle.forward(50)
+
+def move_left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    
+def move_down():
+    turtle.setheading(270)
+    turtle.forward(50)
+
+def move_right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    
+turtle.shape('turtle')
+
+turtle.listen()
+turtle.setheading(90)
+turtle.onkey(move_up, 'w')
+turtle.onkey(move_left, 'a')
+turtle.onkey(move_down, 's')
+turtle.onkey(move_right, 'd')
